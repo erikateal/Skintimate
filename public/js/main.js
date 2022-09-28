544e15170dfe2d482473b335396c61fea0ca13c0
Erika Teal <erikamw2070@yahoo.com>
2022-09-28 13:56:55 -0500

    added YT carousel under resources

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,15 @@
-$(function () {
-  if ($(".owl-2").length > 0) {
-    $(".owl-2").owlCarousel({
-      center: false,
+// Owl Carousel
+
+$(".about-area .owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: false,
+  responsive: {
+    0: {
       items: 1,
-      loop: true,
-      stagePadding: 0,
-      margin: 20,
-      smartSpeed: 1000,
-      autoplay: true,
-      nav: true,
-      dots: true,
-      pauseOnHover: false,
-      responsive: {
-        600: {
-          margin: 20,
-          nav: true,
-          items: 2,
-        },
-        1000: {
-          margin: 20,
-          stagePadding: 0,
-          nav: true,
-          items: 3,
-        },
-      },
-    });
-  }
+    },
+    560: {
+      items: 2,
+    },
+  },
 });
